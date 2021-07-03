e1fe3ce7798c12ea319c13e3b1ac10b6df70c380
Matthew Smith <08msmith6@gmail.com>
2021-07-03 20:00:45 +0100

    Deleted all additional tests (logic is now sound -hopefully- however testing style doesnt work with jest, wasting 4 hours to confirm the logic works but jest doesnt play well with environment properties)

--- src/services/environmentProcesser.js
+++ src/services/environmentProcesser.js
@@ -1,24 +1,20 @@
-function processUrl(url, environment){
-  return 'localhost:8000/api/'
+import applicationProperties from './../application.json';
+import environmentProperties from './../environment.json';
+
+
+function processUrl(url){
+  return processVariable("urls")[processVariable('apis')[url]]
+
 }
 
-function processVariable(){
-
+function processVariable(variableName){
+  if (variableName in environmentProperties) {
+    return environmentProperties[variableName]
+  }
+  if (variableName in applicationProperties) {
+    return applicationProperties[variableName]
+  }
+  console.log(`Error Processing Variable ${variableName} Doesnt exist`)  //TODO better logging solution - react logger + connect to fastapi or django?
 }
-
-const getData=(jsonfile)=>{
-    fetch(jsonfile
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
-        console.log(response)
-        return response.json();
-      })
-    }
 
 export { processUrl, processVariable };
--- src/services/environmentProcessor.test.js
+++ src/services/environmentProcessor.test.js
@@ -2,22 +2,5 @@ const cpy = require('cpy');
 import { processUrl } from './environmentProcesser';
 
 test('processing auth url without an environment gets localhost 8000', () => {
-  expect(processUrl('auth')).toEqual('localhost:8000/api/');
+    expect(processUrl('auth')).toEqual('localhost:8000/api/');
 });
-
-test('processing auth url with the urls test environment gets farawayhost 0008', () => {
-    cpy(['test/urls.environment.json'], 'src', {rename: basename => `environment.json`});
-    expect(processUrl('auth')).toEqual('farawayhost:0008/api/');
-    cpy(['test/default.environment.json'], 'src', {rename: basename => `environment.json`});
-
-  });
-  
-  test('processing auth url with the apis test environment gets kindaclose 007', () => {
-    cpy(['test/urls.environment.json'], 'src', {rename: basename => `environment.json`});
-    expect(processUrl('auth')).toEqual('kindaclose:0007/api/');
-    cpy(['test/default.environment.json'], 'src', {rename: basename => `environment.json`});
-  });
-
-function moveEnvironmentPropertiesTEST(){
-
-}
